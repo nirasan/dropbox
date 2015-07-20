3b659181d5f3c0dfc5e9ac0ede4d0da71cd70e3c
nirasan <nirasan@gmail.com>
2015-07-20 11:18:14 +0900

    ファイルをS3に保存する

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,1 +1,23 @@
 CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['aws_access'],
+    aws_secret_access_key: ENV['aws_secret'],
+    region: 'ap-northeast-1'
+  }
+
+  case Rails.env
+    when 'production'
+      config.fog_directory = 'dropbox-copy-app'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/dropbox-copy-app'
+
+    when 'development'
+      config.fog_directory = 'dropbox-copy-app-dev'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/dropbox-copy-app-dev'
+
+    when 'test'
+      config.fog_directory = 'dropbox-copy-app-test'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/dropbox-copy-app-test'
+  end
+end
